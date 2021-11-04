534d1555f702e7c5e9a5916c1c5b52e206d11b23
martamsrs <91379645+martamsrs@users.noreply.github.com>
2021-11-04 20:22:00 +0000

    atualizado

--- Libraryverse/app/src/main/java/com/example/libraryverse/HomeActivity.java
+++ Libraryverse/app/src/main/java/com/example/libraryverse/HomeActivity.java
@@ -3,15 +3,39 @@ package com.example.libraryverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
-        return true;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        initializeMyListView();
+
     }
+
+    private void initializeMyListView()
+    {
+        ListView myList = (ListView) findViewById(R.id.ListView);
+
+        ArrayList<String> myItems = new ArrayList<String>();
+        myItems.add("Filmes");
+        myItems.add("Livros");
+        myItems.add("Log Out");
+        myItems.add("Profile");
+
+        ArrayAdapter<String> myListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myItems);
+
+        myList.setAdapter(myListAdapter);
+
+    }
+
+
+
 }
