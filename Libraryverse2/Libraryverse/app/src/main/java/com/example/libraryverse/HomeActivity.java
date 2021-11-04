fcfc931c88515a65700c982ce609d5b815b7d2d0
martamsrs <91379645+martamsrs@users.noreply.github.com>
2021-11-04 19:18:48 +0000

    teste

---
+++ Libraryverse2/Libraryverse/app/src/main/java/com/example/libraryverse/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.libraryverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+}
