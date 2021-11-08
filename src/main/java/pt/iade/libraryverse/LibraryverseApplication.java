a74f2fc3791f7c0946094a71d7d713e86627cb08
Ana Bernardino <bernardino.ana490@hotmail.com>
2021-11-08 10:59:34 +0000

    Reorganização do repositorio

---
+++ src/main/java/pt/iade/libraryverse/LibraryverseApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.libraryverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/iade/libraryverse/LibraryverseApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.libraryverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
