abea04ea68d6877beaf355bbe8548abd225b3487
Ana Bernardino <bernardino.ana490@hotmail.com>
2021-11-08 11:16:57 +0000

    correção chavetas

--- LibraryverseInterfaces/app/src/main/java/com/example/libraryverse/HomeActivity.java
+++ LibraryverseInterfaces/app/src/main/java/com/example/libraryverse/HomeActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-<<<<<<< Updated upstream
+
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,22 +16,5 @@ public class HomeActivity extends AppCompatActivity {
         inflater.inflate(R.menu.game_menu, menu);
         return true;
     }
-=======
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-public class HomeActivity extends AppCompatActivity {
-
-
-
-
-
-
-
-
-
->>>>>>> Stashed changes
 }
+
