800b3968d1158fb67c75304beae91ef9f717d427
martamsrs <91379645+martamsrs@users.noreply.github.com>
2021-11-18 11:32:10 +0000

    ola7

--- Interfaces/app/src/main/java/com/example/libraryverse/ItemActivity.java
+++ Interfaces/app/src/main/java/com/example/libraryverse/ItemActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Favmovies extends AppCompatActivity {
+public class ItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favmovies);
+        setContentView(R.layout.activity_item);
     }
 }
