ee043a91bf93f0cd98292490c86e8aa35c3c549b
martamsrs <91379645+martamsrs@users.noreply.github.com>
2021-11-25 12:32:17 +0000

    update android

---
+++ Libraryverse/app/src/main/java/com/example/libraryverse/CreateAccountActivity.java
@@ -0,0 +1,26 @@
+package com.example.libraryverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+    public void clickToHome(View view)
+    {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+
+
+
+    }
+}
