ac169dbf4149fa63cc58ca912d985c2bfd609894
Ana Bernardino <bernardino.ana490@hotmail.com>
2021-12-02 11:44:39 +0000

    erros

--- src/main/java/pt/iade/libraryverse/models/repositories/GenreRepository.java
+++ src/main/java/pt/iade/libraryverse/models/repositories/GenreRepository.java
@@ -3,14 +3,14 @@ package pt.iade.libraryverse.models.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.libraryverse.models.Genre;
 import pt.iade.libraryverse.models.views.GenreView;
 
 public interface GenreRepository extends CrudRepository<Genre,Integer>
 {
-    List<Genre> getGenre(int id); 
-
     String QueryGetItemGenre = 
     "select genre_name genreName, movie_name movieName from genre "+
     "inner join moviegenre on genre_id = mg_genre_id "+
