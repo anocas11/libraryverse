a91b0de870525bc1341b42212437757993ce3b09
Ana Bernardino <bernardino.ana490@hotmail.com>
2021-12-02 16:24:18 +0000

    x

--- src/main/java/pt/iade/libraryverse/models/repositories/MovieRepository.java
+++ src/main/java/pt/iade/libraryverse/models/repositories/MovieRepository.java
@@ -2,11 +2,15 @@ package pt.iade.libraryverse.models.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.libraryverse.models.Movie;
+import pt.iade.libraryverse.models.views.MovienfoView;
 
 public interface MovieRepository extends CrudRepository<Movie,Integer>
 {
-    //String QueryGetMovieInfo = "select movie_name as movieName, movie_duration as duration, movie_date as date, movie_description as description, cu_name as cinematicUniverse " + 
-    //"from movie " + 
-    //"inner join cinematicuniverse on movie_cu_id = cu_id " +
-    //"where movie_id = " + id;
+    String QueryGetMovieInfo = "select movie_name as movieName, movie_duration as duration, movie_date as date, movie_description as description, cu_name as cinematicUniverse " + 
+    "from movie " + 
+    "inner join cinematicuniverse on movie_cu_id = cu_id ";
+
+    @Query(value = QueryGetMovieGenre +
+    " where movie_id=:id", nativeQuery = true)
+    Iterable<MovienfoView> getMovie(@Param("id") int id);
 }
