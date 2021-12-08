85d5571e089c2936a31dc9975ae32041835fa28e
Ana Bernardino <bernardino.ana490@hotmail.com>
2021-12-08 21:21:58 +0000

    espaço

--- src/main/java/pt/iade/libraryverse/models/repositories/MovieRepository.java
+++ src/main/java/pt/iade/libraryverse/models/repositories/MovieRepository.java
@@ -8,7 +8,7 @@ import pt.iade.libraryverse.models.views.MovieInfoView;
 
 public interface MovieRepository extends CrudRepository<Movie,Integer>
 {
-    String QueryGetMovieInfo = "select movie_name as name, movie_duration as duration, movie_date as date, movie_description as description, cu_name as cinematicUniverse, genre_name as genre, movie_poster as poster" +
+    String QueryGetMovieInfo = "select movie_name as name, movie_duration as duration, movie_date as date, movie_description as description, cu_name as cinematicUniverse, genre_name as genre, movie_poster as poster " +
     "from movie " +
     "left join cinematicuniverse on movie_cu_id = cu_id " +
     "inner join movieGenre on mg_movie_id = movie_id " +
